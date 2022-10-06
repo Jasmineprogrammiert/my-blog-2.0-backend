39ed7280ef8b564cde019e36c23f8c82e87e5bea
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2022-10-06 15:49:13 +0800

    Update: CORS

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'https://lieblingsjasmin.com', 'http://localhost:1209' }));
+app.use(
+  cors({ 
+    origin: ['https://lieblingsjasmin.com', 'http://localhost:1209']
+  }));
 
 app.use('/api/blogs', blogRoutes);
 app.use('/api/auth', authRoutes);
