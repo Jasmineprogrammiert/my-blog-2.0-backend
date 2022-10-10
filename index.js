69d4f7264fc15d19accd3260ca6550d4911d2956
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2022-10-10 14:32:26 +0800

    Update: CORS

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 
 app.use('/blogs', blogRoutes);
 app.use('/auth', authRoutes);
