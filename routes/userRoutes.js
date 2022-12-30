f0e3f3e01c3c659adf4b8c9c90f66ab1581dbe75
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2022-12-30 14:08:17 +0800

    Update: auth-related

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,48 +13,6 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// router.put('/:id', async (req, res) => {
-//   if (req.body.userId === req.params.id) {
-//     if (req.body.password) {
-//       const salt = await bcrypt.genSalt(10);
-//       req.body.password = await bcrypt.hash(req.body.password, salt);
-//     }
-//     try {
-//       const updatedUser = await User.findByIdAndUpdate
-//       (
-//         req.params.id,
-//         { $set: req.body },
-//         { new: true }
-//       );
-//       res.status(200).json(updatedUser);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   } 
-//   else {
-//     res.status(401).json('You can only update your account!');
-//   }
-// });
-
-// router.delete('/:id', async (req, res) => {
-//   if (req.body.userId === req.params.id) {
-//     try {
-//       const user = await User.findById(req.params.id);
-//       try {
-//         await Blog.deleteMany({ username: user.username });
-//         await User.findByIdAndDelete(req.params.id);
-//         res.status(200).json('User has been deleted.');
-//       } catch (err) {
-//         res.status(500).json(err);
-//       }
-//     } catch (err) {
-//       res.status(404).json('User not found!');
-//     }
-//   } else {
-//     res.status(401).json('You can only delet your account!');
-//   }
-// });
-
 router.put('/:id', async (req, res) => {
   try {
     const updatedUser = await User.findByIdAndUpdate
@@ -65,7 +23,7 @@ router.put('/:id', async (req, res) => {
     );
     res.status(200).json(updatedUser);
   } catch (err) {
-    res.status(401).json('Authorization failed');
+    res.status(500).json(err);
   }
   // if (req.body.userId === req.params.id) {
   //   if (req.body.password) {
@@ -85,7 +43,7 @@ router.put('/:id', async (req, res) => {
   //   }
   // } 
   // else {
-  //   res.status(401).json('You can only update your account!');
+  //   res.status(401).json('Authorization failed');
   // }
 });
 
@@ -112,8 +70,51 @@ router.delete('/:id', async (req, res) => {
   //     res.status(404).json('User not found!');
   //   }
   // } else {
-  //   res.status(401).json('You can only delet your account!');
+  //   res.status(401).json('Authorization failed');
   // }
 });
+
+// ------------ ORIGINAL ------------
+// router.put('/:id', async (req, res) => {
+//   if (req.body.userId === req.params.id) {
+//     if (req.body.password) {
+//       const salt = await bcrypt.genSalt(10);
+//       req.body.password = await bcrypt.hash(req.body.password, salt);
+//     }
+//     try {
+//       const updatedUser = await User.findByIdAndUpdate
+//       (
+//         req.params.id,
+//         { $set: req.body },
+//         { new: true }
+//       );
+//       res.status(200).json(updatedUser);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   } 
+//   else {
+//     res.status(401).json('Authorization failed');
+//   }
+// });
+
+// router.delete('/:id', async (req, res) => {
+//   if (req.body.userId === req.params.id) {
+//     try {
+//       const user = await User.findById(req.params.id);
+//       try {
+//         await Blog.deleteMany({ username: user.username });
+//         await User.findByIdAndDelete(req.params.id);
+//         res.status(200).json('User has been deleted.');
+//       } catch (err) {
+//         res.status(500).json(err);
+//       }
+//     } catch (err) {
+//       res.status(404).json('User not found!');
+//     }
+//   } else {
+//     res.status(401).json('Authorization failed');
+//   }
+// });
 
 module.exports = router;
